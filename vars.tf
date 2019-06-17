46ad47d42fb499532ef2be72a5fe5240027ac768
Stephen Fox <stevefox@users.noreply.github.com>
2019-06-17 07:15:22 -0500

    Add missing variable

--- vars.tf
+++ vars.tf
@@ -5,3 +5,8 @@ variable "cluster_count" {
 variable "instance_type" {
   description = "The instance type"
 }
+
+variable "lab_username" {
+  description = "The user's lab username. Must be formatted like lab-user-x"
+}
+
