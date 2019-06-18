88f58f7e1f392b81b340ad33ff1a354f6bec6980
Stephen Fox <stevefox@users.noreply.github.com>
2019-06-17 22:33:57 -0500

    sync prod

--- main.tf
+++ main.tf
@@ -13,14 +13,3 @@ resource "aws_instance" "example" {
 
   count         = "${var.cluster_count}"
 }
-
-resource "aws_instance" "new_feature" {
-  ami           = "ami-0565af6e282977273"
-  instance_type = "${var.instance_type}"
-
-  tags = {
-    Name        = "Module 3 Lab 3"
-    User        = "${var.lab_username}"
-    Description = "New feature"
-  }
-}
